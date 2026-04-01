8dd1e8b2b2b223171c7ecc12807e89208113349f
jacques_vco <jacques@vestiairecollective.com>
2026-04-01 12:26:23 +0700

    add posthog analytics

--- apps/web/src/main.jsx
+++ apps/web/src/main.jsx
@@ -3,8 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 
+import posthog from 'posthog-js'
+
+posthog.init('phc_zz4Q6VJETesgBUkeKe8a9asUwbra9qGXgw4ff6zPTxLM', {
+    api_host: 'https://eu.posthog.com',
+})
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <App />
+    </StrictMode>,
 )
